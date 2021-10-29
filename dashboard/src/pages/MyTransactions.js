fe7d920430dfea58a3c02aec39d94f8ec732260d
leedc0101 <leedc0101@naver.com>
2021-10-29 20:35:24 +0900

    Change some..

--- dashboard/src/pages/MyTransactions.js
+++ dashboard/src/pages/MyTransactions.js
@@ -5,79 +5,86 @@ import MyTransactionsSend from "../components/MyTransactionsSend";
 import MyTransactionsReceive from "../components/MyTransactionsReceive";
 import TransactionsChart from "../components/TransactionsChart";
 
-function MySelect({select}) {
-  if (select === 2) {return <MyTransactionsReceive />;}
-  else if (select === 1) {return <MyTransactionsSend />;}
-  else {return <MyTransactionsAll />;}
+function MySelect({ select }) {
+    if (select === 2) {
+        return <MyTransactionsReceive />;
+    } else if (select === 1) {
+        return <MyTransactionsSend />;
+    } else {
+        return <MyTransactionsAll />;
+    }
 }
 
-
 function MyTransactions() {
-  const [index, setIndex] = useState(0);
+    const [index, setIndex] = useState(0);
 
     return (
         <Flex m={10} flexDirection="column" w="full">
-
-
             <Text fontSize="3xl" fontWeight="700">
                 My 거래내역
             </Text>
-
-            {/* 그래프 */}
-
-            <VStack w="full" backgroundColor="white" borderRadius="5px" p={5} marginBottom={5} marginTop={5}>
-            <TransactionsChart/>
+            <VStack
+                w="full"
+                backgroundColor="white"
+                borderRadius="5px"
+                p={5}
+                marginBottom={5}
+                marginTop={5}
+                borderRadius={20}
+            >
+                <TransactionsChart />
             </VStack>
-
-
-
             <HStack>
                 <Box
                     as="button"
-                    backgroundColor={index===0?"#4318FF":"white"}
-                    color={index===0?"white":"black"}
+                    backgroundColor={index === 0 ? "#4318FF" : "white"}
+                    color={index === 0 ? "white" : "black"}
                     borderRadius="3px"
                     width="60px"
                     fontWeight="bold"
-                    onClick={()=>{
-                      setIndex(0);
-
-                    }
-                  }
+                    onClick={() => {
+                        setIndex(0);
+                    }}
                 >
                     ALL
                 </Box>
                 <Box
                     as="button"
-                    backgroundColor={index===1?"#4318FF":"white"}
-                    color={index===1?"white":"black"}
+                    backgroundColor={index === 1 ? "#4318FF" : "white"}
+                    color={index === 1 ? "white" : "black"}
                     borderRadius="3px"
                     width="60px"
                     fontWeight="bold"
-                    onClick={()=>{
-                      setIndex(1);
+                    onClick={() => {
+                        setIndex(1);
                     }}
                 >
                     보낸 내역
                 </Box>
                 <Box
                     as="button"
-                    backgroundColor={index===2?"#4318FF":"white"}
-                    color={index===2?"white":"black"}
+                    backgroundColor={index === 2 ? "#4318FF" : "white"}
+                    color={index === 2 ? "white" : "black"}
                     borderRadius="3px"
                     width="60px"
                     fontWeight="bold"
-                    onClick={()=>{setIndex(2);}}
+                    onClick={() => {
+                        setIndex(2);
+                    }}
                 >
                     받은 내역
                 </Box>
             </HStack>
 
-            <VStack w="full" backgroundColor="white" borderRadius="5px" p={5} marginTop={5}>
-              <MySelect select={index}/>
+            <VStack
+                w="full"
+                backgroundColor="white"
+                borderRadius={20}
+                p={5}
+                marginTop={5}
+            >
+                <MySelect select={index} />
             </VStack>
-
-            {/* 거래내역 */}
         </Flex>
     );
 }
