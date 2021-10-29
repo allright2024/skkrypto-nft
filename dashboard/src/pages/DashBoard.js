caa8611b2cb56b96ff8dddbd520bed7ba9549f36
leedc0101 <leedc0101@naver.com>
2021-10-29 20:34:45 +0900

    Add AccountInfo

--- dashboard/src/pages/DashBoard.js
+++ dashboard/src/pages/DashBoard.js
@@ -9,12 +9,14 @@ import {
 import TotalPoint from "../components/TotalPoint";
 import PointStatus from "../components/PointStatus";
 import WalletConnect from "../components/WalletConnect";
+import AccountInfo from "../components/AccountInfo";
 
 function DashBoard() {
     const [isLessThan1070] = useMediaQuery("(max-width:1070px)");
     return (
         <Flex wrap="wrap" w="full" h="full" justify="space-evenly" bg="#E5E5E5">
             <Flex
+                flexGrow="1"
                 justify="space-between"
                 p={5}
                 wrap="wrap"
@@ -27,12 +29,11 @@ function DashBoard() {
                 <PointStatus />
                 <PointStatus />
             </Flex>
-            <VStack
-                p={5}
-                minW={isLessThan1070 ? "350px" : 0}
-                w={isLessThan1070 ? "full" : "300px"}
-            >
+            <VStack p={5} minW={isLessThan1070 ? "350px" : 0} flexGrow="1">
                 <WalletConnect />
+                <VStack p={5} w="full" bg="white" borderRadius={20}>
+                    <AccountInfo />
+                </VStack>
             </VStack>
         </Flex>
     );
